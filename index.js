aabced5219344a58c1a26686a4d7f900ae3618dd
Kristen Doucette <cosmicnoir90@gmail.com>
2019-05-31 17:00:23 -0600

    Fix large screen hide-display issue by adding seperate divs for display: none that no longer interfere with display: flex.

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ handleAll();
 function handleResume(){
     // responsible for removing "hidden" class from "resume" class section, and hidding class "home".
     $('.js-skills').click(function(event){
-        $('.js-resume').removeClass('js-hidden');
-        $('.js-home').addClass('js-hidden');
+        $('.js-hideMe2').removeClass('js-hidden');
+        $('.js-hideMe1').addClass('js-hidden');
         console.log('`handleResume` ran');
         handleFadeIn();
     });
@@ -20,14 +20,14 @@ function handleResume(){
 function handleHome(){
     // responsible for removing "hidden" from class "home", and adding class "hidden" to "resume" class section.
     $('.begin').click(function(event){
-        $('.js-resume').addClass('js-hidden');
-        $('.js-home').removeClass('js-hidden');
+        $('.js-hideMe2').addClass('js-hidden');
+        $('.js-hideMe1').removeClass('js-hidden');
         console.log('`handleHome` ran');
     });
 }
 
 function handleFadeIn(){
-    // should re-initiate aos-data specs.
+    // should initiate AOS for newly revealed section
     $('.js-fadeIn').attr('data-aos', 'zoom-in');
     AOS.init({
         duration: 2000,
