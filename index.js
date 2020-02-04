93c9fe147a98df2d4b192d66e71f17897d13f5a6
Kristen Doucette <cosmicnoir90@gmail.com>
2020-02-03 19:51:02 -0700

    Create mobile menu and fix broken link

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 function handleAll() {
-    // Responsible for inital run of any functions
-    handleResume();
-    handleHome();
-    
+  handleClickMobile();
 }
 
 handleAll();
 
-function handleResume(){
-    // responsible for removing "hidden" class from "resume" class section, and hidding class "home".
-    $('.js-skills').click(function(event){
-        $('.js-hideMe2').removeClass('js-hidden');
-        $('.js-hideMe1').addClass('js-hidden');
-        console.log('`handleResume` ran');
-        handleFadeIn();
-    });
+// Responsible for revealing and mobile menu
+function handleClickMobile() {
+  $(".mobileMenu").click(function(e) {
+    $(".mobileNav").toggleClass("hidden");
+  });
 }
-
-function handleHome(){
-    // responsible for removing "hidden" from class "home", and adding class "hidden" to "resume" class section.
-    $('.begin').click(function(event){
-        $('.js-hideMe2').addClass('js-hidden');
-        $('.js-hideMe1').removeClass('js-hidden');
-        console.log('`handleHome` ran');
-    });
-}
-
-function handleFadeIn(){
-    // should initiate AOS for newly revealed section
-    $('.js-fadeIn').attr('data-aos', 'zoom-in');
-    AOS.init({
-        duration: 2000,
-    });
-}
-
-// Unclear how to get AOS to re-initiate when clicking links additional times.
